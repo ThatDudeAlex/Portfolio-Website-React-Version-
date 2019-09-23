e38f3dbbf71df3f17c373a67a3e3f6a068e2de60
Jose A Nunez <josenunez1692@gmail.com>
2019-09-22 22:36:08 -0400

    nav bar works great!

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from "react-scroll";
-import { Link as RouteLink } from 'react-router-dom';
 import '../css/test.css';
+import { Link } from "react-scroll";
+
 
 class Navbar extends Component {
     constructor(props) {
@@ -36,121 +36,120 @@ class Navbar extends Component {
         return (
             <nav id="navbar" className="collapsible-menu" >
                 <div className="nav-content" >
-
-                    <div className="toggle">
-                        <i className="fa fa-bars" onClick={this.handleClick}></i>
-                    </div>
-
-
+                    <i className="fa fa-bars toggle" onClick={this.handleClick}></i>
                     <ul className="nav-items" ref={this.myRef}>
-                            <li onClick={this.handleClick}>
+
+                        <span className="group-1">
+                            <li>
                                 <Link
-                                activeClass="active"
-                                to="aboutComponent"
-                                spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={1000}
-                                className="nav-menu-link"
+                                    activeClass="active"
+                                    to="aboutComponent"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-300}
+                                    duration={1000}
+                                    className="nav-menu-link"
                                 >
-                                    About
+                                    <span onClick={this.handleClick}>About</span>
                                 </Link>
                             </li>
 
-                            <li onClick={this.handleClick}>
+                            <li>
                                 <Link
-                                activeClass="active"
-                                to="expComponent"
-                                spy={true}
-                                smooth={true}
-                                offset={-50}
-                                duration={1000}
-                                className="nav-menu-link"
+                                    activeClass="active"
+                                    to="expComponent"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-50}
+                                    duration={1000}
+                                    className="nav-menu-link"
                                 >
-                                    Experience
+                                    <span onClick={this.handleClick}>Experience</span>
                                 </Link>
                             </li>
 
+                            <li>
+                                <Link
+                                    activeClass="active"
+                                    to="projectComponent"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-80}
+                                    duration={1000}
+                                    className="nav-menu-link"
+                                >
+                                    <span onClick={this.handleClick}>Projects</span>
+                                </Link>
+                            </li>
 
-                            <li onClick={this.handleClick}>
+                            <li>
                                 <Link
-                                activeClass="active"
-                                to="projectComponent"
-                                spy={true}
-                                smooth={true}
-                                offset={-50}
-                                duration={1000}
-                                className="nav-menu-link"
+                                    activeClass="active"
+                                    to="skillsComponent"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={1000}
+                                    className="nav-menu-link"
                                 >
-                                    Projects
+                                    <span onClick={this.handleClick}>Skills</span>
                                 </Link>
                             </li>
-                            
-                            <li onClick={this.handleClick}>
+
+                            <li>
                                 <Link
-                                activeClass="active"
-                                to="skillsComponent"
-                                spy={true}
-                                smooth={true}
-                                offset={-30}
-                                duration={1000}
-                                className="nav-menu-link"
+                                    activeClass="active"
+                                    to="contactComponent"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={0}
+                                    duration={1000}
+                                    className="nav-menu-link"
                                 >
-                                    Skills
+                                    <span onClick={this.handleClick}>Contact</span>
                                 </Link>
                             </li>
-                            
-                            <li onClick={this.handleClick}>
+                        </span>
+
+                        <span className="group-2">
+                            <li>
                                 <Link
-                                activeClass="active"
-                                to="contactComponent"
-                                spy={true}
-                                smooth={true}
-                                offset={0}
-                                duration={1000}
-                                className="nav-menu-link"
+                                    activeClass="active"
+                                    to=""
+                                    spy={true}
+                                    className="nav-menu-link"
                                 >
-                                    Contact
+                                    <span onClick={this.handleClick}>Bio</span>
                                 </Link>
                             </li>
-                            
-                            <li onClick={this.handleClick} className="link-6">
+
+                            <li>
                                 <Link
-                                activeClass="active"
-                                to=""
-                                spy={true}
-                                className="nav-menu-link"
+                                    activeClass="active"
+                                    to=""
+                                    spy={true}
+                                    className="nav-menu-link"
                                 >
-                                    Bio
+                                    <span onClick={this.handleClick}>Blog (Soon)</span>
                                 </Link>
                             </li>
-                            
-                            <li onClick={this.handleClick}>
+
+                            <li>
                                 <Link
-                                activeClass="active"
-                                to=""
-                                spy={true}
-                                className="nav-menu-link"
+                                    activeClass="active"
+                                    to=""
+                                    spy={true}
+                                    className="nav-menu-link"
                                 >
-                                    Blog (Soon)
+                                    <span onClick={this.handleClick}>Resume</span>
                                 </Link>
                             </li>
-                            
-                            <li onClick={this.handleClick}>
-                                <Link
-                                activeClass="active"
-                                to=""
-                                spy={true}
-                                className="nav-menu-link"
-                                >
-                                    Resume
-                                </Link>
-                            </li>
+                        </span>
                     </ul>
                 </div>
             </nav>
-                );
-            }
-        }
-        
+        );
+    }
+}
+
 export default Navbar;
